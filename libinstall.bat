423acb27dbfb0611b481b16d9d2d609069e4aca9
Wong Shouhao <42537782+wangshouh@users.noreply.github.com>
2021-05-24 10:22:05 +0800

    Python lib install

---
+++ libinstall.bat
@@ -0,0 +1,6 @@
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pip -U
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple requests
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple Pillow
+echo library  install finish
+pause
+exit
